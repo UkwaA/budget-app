66d3e6a2d9583de8d049e9ead17d0f53f3db8f52
Ukwa <taunfon@gmail.com>
2021-01-23 12:07:00 -0800

    landing page in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'budget-app';
+
+  ngOnInit(){
+    
+  }
+
+  buttonClicked(){
+    document.getElementById('title-box').classList.remove('slide-in-blurred-bottom');
+    document.getElementById('title-box').classList.add('slide-in-blurred-top');
+  }
 }
